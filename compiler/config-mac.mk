58c627e561edada6da97c2a3bee5c71ebc623dc5
wassila11 <wassila.chtioui@edu.isetcom.com>
2025-03-10 10:01:32 +0100

    mac config

---
+++ compiler/config-mac.mk
@@ -0,0 +1,5 @@
+# the ANTLRJAR part below is copied from /usr/bin/antlr4
+ANTLRINC=/opt/homebrew/Cellar/antlr4-cpp-runtime/4.13.2/include/antlr4-runtime
+ANTLRLIB=/opt/homebrew/lib/libantlr4-runtime.a
+ANTLRJAR=~/Downloads/antlr-4.13.2-complete.jar
+ANTLR=java -jar $(ANTLRJAR)
